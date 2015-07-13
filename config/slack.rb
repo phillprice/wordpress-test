f6d954cc3542474b6d077d48d23415c1c2fc39cd
Phill Price <phill@bostonivy.co>
2015-07-13 12:55:52 +0100

    swap token for placeholder

--- config/slack.rb
+++ config/slack.rb
@@ -4,7 +4,7 @@
 
 # Required
 set :slack_subdomain, 'bostonivy' # if your Slack subdomain is example.slack.com
-set :slack_url, 'SFcbqBKzwLR7pFeGawfLM8BE' # https://my.slack.com/services/new/incoming-webhook
+set :slack_url, 'slack_token_swap' # https://my.slack.com/services/new/incoming-webhook
 
 # Optional
 set :slack_channel, '#general'
